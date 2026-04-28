2539c0945d81db01b90b571b7c590f451af062d0
Vikas Rana <vicay0001@gmail.com>
2026-04-28 22:43:39 +0530

    some correction

--- app/src/main/java/me/vikas/experimentLab/scalable_design/repository_impl/RemoteNewsSourceImpl.kt
+++ app/src/main/java/me/vikas/experimentLab/scalable_design/repository_impl/RemoteNewsSourceImpl.kt
@@ -3,7 +3,8 @@ package me.vikas.experimentLab.scalable_design.repository_impl
 import me.vikas.experimentLab.scalable_design.repository.NewsApiRepository
 
 class RemoteNewsSourceImpl : NewsApiRepository {
-    override fun getRemoteNews(): String {
+
+    override fun getNews(): String {
         return "Remote News"
     }
 }
