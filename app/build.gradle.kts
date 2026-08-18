430f4591b9505997737f32a5737b719b772aa23b
Vikas Rana <vicay0001@gmail.com>
2026-08-18 23:35:00 +0530

    second practice for PR

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -95,7 +95,7 @@ fun renameApkFile(){
                     apk.renameTo(newFile)
                     println("Renamed to ${newFile.name}")
                 } else {
-                    println("Apk Not Found.")
+                    println("Apk Found.")
                 }
 
             }
